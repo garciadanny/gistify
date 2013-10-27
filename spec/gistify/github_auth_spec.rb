94f4bd70dd33fefdde0f9644266002396fef07d4
Danny Garcia <dannygarcia.me@gmail.com>
2013-10-26 23:20:49 -0600

    GithubAuth.login Creates an oauth token for the client

--- spec/gistify/github_auth_spec.rb
+++ spec/gistify/github_auth_spec.rb
@@ -2,22 +2,18 @@ require 'spec_helper'
 
 module Gistify
   describe GithubAuth do
+    context 'given a valid username and password' do
+      let(:username) { 'mrsquarepants' }
+      let(:password) { 'password1' }
 
-    describe '.login' do
-
-      context 'given a valid username and password' do
-        let(:username)   { 'mrsquarepants' }
-        let(:password)   { 'password1' }
-
-        it 'creates a oauth token for the user' do
-          VCR.use_cassette('githubauth_login') do
-            token = GithubAuth.login username, password
-            expect(token).to_not be_nil
-          end
+      it 'creates an oauth token for the client' do
+        VCR.use_cassette('synopsis') do
+          token = GithubAuth.login username, password
+          expect(token).to_not be_nil
         end
+      end
 
-        it 'saves the token to ~/gistify-USERNAME' do
-        end
+      it 'saves the token to ~/.gistify-USERNAME' do
       end
     end
   end
