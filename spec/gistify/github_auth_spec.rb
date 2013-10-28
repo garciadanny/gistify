959f14825ff501c4179ca2653504ca9a617c110e
Danny Garcia <dannygarcia.me@gmail.com>
2013-10-27 21:33:05 -0600

    GithubAuth.login saves the client token to ~/.gistify-USERNAME

--- spec/gistify/github_auth_spec.rb
+++ spec/gistify/github_auth_spec.rb
@@ -9,11 +9,21 @@ module Gistify
       it 'creates an oauth token for the client' do
         VCR.use_cassette('githubauth-login') do
           token = GithubAuth.login username, password
+
           expect(token).to_not be_nil
+          expect(token).to be_a String
         end
       end
 
       it 'saves the token to ~/.gistify-USERNAME' do
+        VCR.use_cassette('githubauth-login') do
+          token = GithubAuth.login username, password
+
+          file_path = File.join(Dir.home, "/.gistify-#{username}")
+          file = File.open file_path
+
+          expect(file.read).to eq token
+        end
       end
     end
   end
