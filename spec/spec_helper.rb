f85d0c51ccaa5fab72de4815146cd83936bb786f
Danny Garcia <dannygarcia.me@gmail.com>
2013-10-27 22:47:26 -0600

    include HelperMethods

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,10 +5,15 @@ $:.unshift File.expand_path("../../spec", __FILE__)
 
 require 'gistify'
 require 'vcr'
+require 'support/helper_methods'
 
 spec_dir = File.join(FileUtils.pwd, 'spec/fixtures')
-
 VCR.configure do |c|
   c.cassette_library_dir = File.join(spec_dir, 'vcr_cassettes')
   c.hook_into :webmock
+end
+
+
+RSpec.configure do |c|
+  c.include Gistify::HelperMethods
 end
