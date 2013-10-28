0d8dd6d4d43bff55a2281beba7f0f02be76e7ea6
Danny Garcia <dannygarcia.me@gmail.com>
2013-10-27 22:46:54 -0600

    add authenticate_client helper method, locate token in ~/.gistify

--- spec/gistify/github_auth_spec.rb
+++ spec/gistify/github_auth_spec.rb
@@ -1,28 +1,30 @@
 require 'spec_helper'
 
 module Gistify
+
   describe GithubAuth do
-    context 'given a valid username and password' do
-      let(:username) { 'mrsquarepants' }
-      let(:password) { 'password1' }
 
-      it 'creates an oauth token for the client' do
-        VCR.use_cassette('githubauth-login') do
-          token = GithubAuth.login username, password
+    describe '.login' do
 
-          expect(token).to_not be_nil
-          expect(token).to be_a String
+      context 'given a valid username and password' do
+        it 'creates an oauth token for the client' do
+          VCR.use_cassette('githubauth-login') do
+            token = authenticate_client
+
+            expect(token).to_not be_nil
+            expect(token).to be_a String
+          end
         end
-      end
 
-      it 'saves the token to ~/.gistify-USERNAME' do
-        VCR.use_cassette('githubauth-login') do
-          token = GithubAuth.login username, password
+        it 'saves the token to ~/.gistify-USERNAME' do
+          VCR.use_cassette('githubauth-login') do
+            token = authenticate_client
 
-          file_path = File.join(Dir.home, "/.gistify-#{username}")
-          file = File.open file_path
+            file_path = File.join(Dir.home, '/.gistify')
+            file = File.open file_path
 
-          expect(file.read).to eq token
+            expect(file.read).to eq token
+          end
         end
       end
     end
