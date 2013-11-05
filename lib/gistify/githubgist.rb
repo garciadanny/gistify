013bddaa933c0c5f1c03f0ebc5404dd798e670e2
Danny Garcia <dannygarcia.me@gmail.com>
2013-11-04 22:54:32 -0700

    GithubGist .parse .print_response

--- lib/gistify/githubgist.rb
+++ lib/gistify/githubgist.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'hirb'
 
 module Gistify
   class GithubGist
@@ -9,8 +10,7 @@ module Gistify
       response = get GISTS_URL, headers: {
         'Authorization' => "token #{user_token}"
       }
-      p response
-      response
+      print_response parse(response)
     end
 
   private
@@ -24,6 +24,20 @@ module Gistify
 
     def self.home_directory
       File.expand_path '~/.gistify'
+    end
+
+    def self.parse response
+      response.map do |gist|
+        url         = gist['html_url']
+        file_names  = gist['files'].map(&:first)
+        description = gist['description']
+
+        {'Filename(s)' => file_names, 'Description' => description, 'URL' => url}
+      end
+    end
+
+    def self.print_response parsed_response
+      puts Hirb::Helpers::Table.render parsed_response, number: true, resize: false
     end
   end
 end
