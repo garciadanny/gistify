2716369f8f49596263d32628912c427ba111a307
Danny Garcia <dannygarcia.me@gmail.com>
2013-11-06 20:58:50 -0700

    add constants | GitHub URI and token location

--- lib/gistify.rb
+++ lib/gistify.rb
@@ -4,5 +4,7 @@ require 'gistify/githubauth'
 require 'gistify/githubgist'
 
 module Gistify
-  # Your code goes here...
+  TOKEN_LOCATION = File.expand_path '~/.gistify'
+  GISTS_URL = 'https://api.github.com/gists'
+  AUTHORIZATIONS_URL = 'https://api.github.com/authorizations'
 end
