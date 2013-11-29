b8a472b58f2dc395cdc1fb5729ce9b55b1f010a2
Danny Garcia <dannygarcia.me@gmail.com>
2013-11-29 16:33:30 -0700

    change GIST_URL TO GISTS_API_URL

--- lib/gistify.rb
+++ lib/gistify.rb
@@ -1,11 +1,10 @@
 require "gistify/version"
 
-require 'gistify/cli'
 require 'gistify/githubauth'
 require 'gistify/githubgist'
 
 module Gistify
   TOKEN_LOCATION = File.expand_path '~/.gistify'
-  GISTS_URL = 'https://api.github.com/gists'
+  GISTS_API_URL = 'https://api.github.com/gists'
   AUTHORIZATIONS_URL = 'https://api.github.com/authorizations'
 end
