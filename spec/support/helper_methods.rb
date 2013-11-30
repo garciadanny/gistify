ae90b76dc3618eaef071578773525bcafa081c26
Danny Garcia <dannygarcia.me@gmail.com>
2013-11-29 19:30:37 -0700

    add authenticate_client vcr cassette

--- spec/support/helper_methods.rb
+++ spec/support/helper_methods.rb
@@ -2,7 +2,7 @@ module Gistify
   module HelperMethods
 
     def authenticate_client
-      VCR.use_cassette('githubauth-login') do
+      VCR.use_cassette('authenticate_client') do
         GithubAuth.login ENV['GITHUB_USERNAME'], ENV['GITHUB_PASSWORD']
       end
     end
