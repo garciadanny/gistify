74c8469423dab3e4fc2c0f504eb0390072794a52
Danny Garcia <dannygarcia.me@gmail.com>
2013-11-29 19:27:28 -0700

    GithubAuth.invalid_credentials

--- lib/gistify/githubauth.rb
+++ lib/gistify/githubauth.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'pry'
 
 module Gistify
   class GithubAuth
@@ -6,21 +7,32 @@ module Gistify
     headers 'User-Agent' => 'garciadanny'
 
     def self.login username, password
-      post_body = { scopes: ['gist'] }
+      post_body = {scopes: ['gist']}
       response = post AUTHORIZATIONS_URL, {
-        body: post_body.to_json,
-        basic_auth: { username: username, password: password }
+          body: post_body.to_json,
+          basic_auth: {username: username, password: password}
       }
       token = response.parsed_response['token']
-      save_token token
+      unless response['message'] == 'Bad credentials'
+        save_token token
+      else
+        invalid_credentials
+      end
     end
 
-  private
+    private
     def self.save_token user_token
       File.open TOKEN_LOCATION, 'w', 0600 do |f|
         f.write user_token
       end
       user_token
     end
+
+    def self.invalid_credentials
+      raise InvalidCredentialsError,
+            "The Username/Password combination you entered was invalid. Please make sure you didn't misspell anything."
+    end
+  end
+  class InvalidCredentialsError < StandardError;
   end
 end
