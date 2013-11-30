caf0960999e0f321b0398b07752f2dec7b6d5d48
Danny Garcia <dannygarcia.me@gmail.com>
2013-11-29 23:22:51 -0700

    #list, #login, #file

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,8 +1,33 @@
 require 'thor'
+require 'io/console'
 
 module Gistify
   class CLI < Thor
+    package_name "Gistify"
+    desc 'login', 'Creates an oauth token'
+    def login
+      GithubAuth.login username, password
+    end
 
+    desc 'list', 'Lists all your gists'
+    def list
+      GithubGist.list
+    end
+
+    desc 'file [FILENAME]', 'Creates a gist of the specified file'
+    method_option :description, type: :string, aliases: '-d'
+    def file file_name
+      GithubGist.file file_name, options[:description]
+    end
+
+    private
+    def username
+      ask 'GitHub username: '
+    end
+
+    def password
+      $stdin.noecho { ask 'Password: ' }
+    end
   end
 end
 
